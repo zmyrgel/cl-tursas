96ba8a453f6b8e87b5e40c7195d3eeb43477d06b
Timo Myyrä <timo.myyra@gmail.com>
2012-01-14 12:44:16 +0200

    remove # from package name

--- state0x88/movegen0x88.lisp
+++ state0x88/movegen0x88.lisp
@@ -12,7 +12,7 @@
 ;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 ;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-(in-package #:tursas.state0x88)
+(in-package :tursas.state0x88)
 
 (define-constant +rook-directions+ (list +north+ +south+ +east+ +west+)
   :test 'equal)
