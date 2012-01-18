94c2da5d2b19669fca82849095dedc03f0230dd9
Timo Myyrä <timo.myyra@gmail.com>
2012-01-18 22:48:11 +0200

    add board tests

--- tests/state0x88.lisp
+++ tests/state0x88.lisp
@@ -17,20 +17,83 @@
 (def-suite state0x88-suite :description "State0x88 package test suite.")
 (in-suite state0x88-suite)
 
-;; (def-fixture person-schema []
-;;   (with-connection hdb
-;;     (try
-;;      (do-commands "create table person (name varchar(255), age integer)")
-;;      (insert-values :person [:name] ["bill"] ["joey"])
-;;      (with-query-results results [query]
-;;        (test-body))
-;;      (finally
-;;       (do-commands "drop schema public cascade")))))
+(def-fixture with-board [fen]
+  (let ((board (State0x88-board (fen->state fen))))
+    (test-body)))
 
-;; (deftest test-ideal
-;;   (with-fixture person-schema []
-;;     (is (= "bill" (:name (first results))))
-;;     (is (= "joey" (:name (second results)))))))
+;;; BOARD TESTS ;;;
 
-(test dummy
-  (is (= 1 1)))
+(deftest board-tests
+    (with-fixture board-setup ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"]
+      (is-false (white-piece-p +black-pawn+))
+      (is-false (white-piece-p +black-king+))
+      (is-true (white-piece-p +white-pawn+))
+      (is-false (white-piece-p +empty-square+))
+      (is-true (black-piece-p +black-pawn+))
+      (is-false (black-piece-p +white-pawn+))
+      (is-false (black-piece-p +empty-square+))
+
+      (is-true (occupied-by-p (board #x10 +white+)))
+      (is-true (occupied-by-p (board #x00 +white+)))
+      (is-true (occupied-by-p (board #x70 +black+)))
+      (is-false (occupied-by-p (board #x70 +white+)))
+      (is-false (occupied-by-p (board #x40 +white+)))
+
+      (is (= (opponent +white+) +black+))
+      (is (= (opponent +black+) +white+))
+
+      ;; (is (promotionp piece move) )
+
+      (is (piece-name +white-king+) #\K)
+      (is (piece-name +black-queen+) #\q)
+      (is (piece-name +white-pawn+) #\p)
+      (is (piece-name +black-king+) #\k)
+
+      (is (piece-value #\K) +white-king+)
+      (is (piece-value #\q) +black-queen+)
+      (is (piece-value #\p) +white-pawn+)
+      (is (piece-value #\k) +black-king+)
+
+      (is-true (board-index-p #x10))
+      (is-true (board-index-p #x0))
+      (is-true (board-index-p #x77))
+      (is-false (board-index-p #x78))
+      (is-false (board-index-p #x128))
+
+      (is-true (empty-square-p board #x20))
+      (is-false (empty-square-p board #x00))
+      (is-true (empty-square-p board #x57))
+
+      (is (column #x10) 1)
+      (is (column #x0) 0)
+      (is (column #x60) 6)
+
+      (is (row #x10) 0)
+      (is (row #x22) 2)
+      (is (row #x42) 2)
+      (is (row #x46) 6)
+
+      (is-true (same-column-p #x10 #x20))
+      (is-true (same-column-p #x10 #x30))
+      (is-false (same-column-p #x20 #x34))
+
+      (is-true (same-row-p #x10 #x50))
+      (is-true (same-row-p #x10 #x53))
+      (is-false (same-row-p #x11 #x66))
+
+      (is (square-color #x10) +white+)
+      (is (square-color #x11) +black+)
+
+      (is-true (same-color-p #x10 #x12))
+
+      (is-true (board-occupied-p board #x10))
+
+      (is (board-ref board #x10) +white-pawn+)
+      (is (board-ref board #x30) +empty-square+)
+
+      (is-false (fill-square! board #x45 20))
+
+      ;; fill-square!
+      ;; clear-square!
+
+      (is (king-index board +white+) #44))
