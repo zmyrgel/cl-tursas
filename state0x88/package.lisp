582d0a2376d1ec673fcf83cec098ee202832ea75
Timo Myyrä <timo.myyra@gmail.com>
2012-03-10 22:12:40 +0200

    no need to import hash functions

--- state0x88/package.lisp
+++ state0x88/package.lisp
@@ -13,11 +13,11 @@
 ;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 (defpackage :tursas.state0x88
-  (:documentation "Package provides 0x88-based implementation.")
+  (:documentation "Package provides 0x88-based chess board implementation.")
   (:use :cl)
   (:import-from :cl-utilities :split-sequence)
   (:import-from :alexandria
-   :define-constant :when-let :if-let :curry :hash-table-keys :hash-table-values :copy-array :copy-hash-table :hash-table-alist)
+   :define-constant :when-let :if-let)
   (:import-from :tursas.utils
    :valid-coord-p :split-move :coordinate-string-p
    :san-string-p :move-string-p :fen->ascii
