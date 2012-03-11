e461814da7a977150eaba32af3391bd2f154f003
Timo Myyrä <timo.myyra@gmail.com>
2012-03-11 11:37:57 +0200

    fix with-output-to-string calls in tursas tests

--- tests/tursas.lisp
+++ tests/tursas.lisp
@@ -19,14 +19,14 @@
 
 (test repl-test
   "Test game repl functionality."
-  (is (with-output-to-string (*STANDARD-OUTPUT*)
+  (is (with-output-to-string (s)
         (game-print (list "foo" "bar")))
       "foo\nbar")
-  (is (with-output-to-string (*STANDARD-OUTPUT*)
+  (is (with-output-to-string (s)
         (game-print "foo"))
       "foo")
-  (is (with-output-to-string (*STANDARD-OUTPUT*)
+  (is (with-output-to-string (s)
         (game-print 200))
       "200")
-  (is-false (with-output-to-string (*STANDARD-OUTPUT*)
+  (is-false (with-output-to-string (s)
               (game-print nil))))
