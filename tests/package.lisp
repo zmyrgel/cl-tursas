3eb8158530c7ce687c9b99766dd1ccb16b04368f
Timo Myyrä <timo.myyra@gmail.com>
2012-03-11 11:37:00 +0200

    clean up tests package

--- tests/package.lisp
+++ tests/package.lisp
@@ -19,16 +19,10 @@
    :valid-coord-p :split-move :coordinate-string-p
    :san-string-p :move-string-p :fen->ascii
    :split-on :expand-digits :compact-item :str :string-indexed)
-  ;; (:import-from :cl-ppcre
-  ;;  :scan :register-groups-bind)
-  ;; (:import-from :alexandria
-  ;;  :define-constant :iota :curry :if-let :when-let :alist-hash-table)
-  ;; (:import-from :cl-utilities
-  ;;  :split-sequence)
-  ;; (:import-from :tursas.state0x88
-  ;;  :coord->move :move->coord :from :to :promotion
-  ;;  :allowedp :occupiedp :blackp :whitep :checkp :matep :drawp
-  ;;  :state->fen :result :legal-states :legal-moves :apply-move
-  ;;  :turn :perft :dynamicp :full-moves :evaluate :last-move
-  ;;  :game-score :game-end-p :fen->state)
+  (:import-from :tursas.state0x88
+   :coord->move :move->coord :from :to :promotion
+   :allowedp :occupiedp :blackp :whitep :checkp :matep :drawp
+   :state->fen :result :legal-states :legal-moves :apply-move
+   :turn :perft :dynamicp :full-moves :evaluate :last-move
+   :game-score :game-end-p :fen->state)
   (:export :run-tests))
