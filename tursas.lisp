6b1c636bda02590f9974b6ec0f105ac8deaca046
Timo Myyrä <timo.myyra@wickedbsd.net>
2018-02-21 17:43:08 +0200

    move alpha-beta function to tursas.lisp and delete search.lisp

--- tursas.lisp
+++ tursas.lisp
@@ -101,6 +101,31 @@
       (princ *game-state* s))
     nil))
 
+(defun alpha-beta (alpha beta depth eval-fn state)
+  "Find the best state, from initial state by searching to given depth
+   and backing up values using cutoff whenever possible.
+   Based on alpha-beta presented in PAIP"
+  (declare (fixnum alpha beta depth)
+           (optimize (speed 3)
+                     (safety 0)))
+  (labels ((f (states best-state ac depth)
+             (declare (fixnum ac depth))
+             (if (null states)
+                 (values ac best-state)
+                 (let ((value (the fixnum (- (the fixnum (alpha-beta (the fixnum (- beta))
+                                                                     (the fixnum (- ac))
+                                                                     depth
+                                                                     eval-fn
+                                                                     (first states)))))))
+                   (cond ((>= ac beta) (values ac best-state))
+                         ((> value ac) (f (rest states) (first states) value depth))
+                         (t (f (rest states) best-state ac depth)))))))
+    (cond ((game-end-p state) (values (game-score state) nil))
+          ((zerop depth) (values (funcall (the function eval-fn) state) nil))
+          (t (if-let ((children (legal-states state)))
+               (f children (first children) alpha (1- depth))
+               (f nil nil (funcall (the function eval-fn) state) 0))))))
+
 (defparameter *search-fn*
   (lambda (state)
     (alpha-beta most-negative-fixnum most-positive-fixnum (get-option :depth-limit) #'evaluate state)))
