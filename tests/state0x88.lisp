ba33a5d8f9cde3ad1b89189bd523b80c257a5142
Timo Myyrä <timo.myyra@wickedbsd.net>
2018-02-21 08:54:10 +0200

    update state tests a bit

--- tests/state0x88.lisp
+++ tests/state0x88.lisp
@@ -14,11 +14,11 @@
 
 ;; test that white can make normal castling move
 (test castling-move-tests
-  (let ((state (fen->state "r3k3/pp1qpppr/n1ppbn1p/8/2B5/BP1Q1P1N/P1P1P1PP/RN2K2R w KQq - 4 7")))
+  (let ((state (tursas.state0x88::fen->state "r3k3/pp1qpppr/n1ppbn1p/8/2B5/BP1Q1P1N/P1P1P1PP/RN2K2R w KQq - 4 7")))
     (is-true
      (member "e1g1" (mapcar #'move->coord
                             (legal-moves state))
-             :test #'string=))))
+             :test 'string=))))
 
 ;; test that check is detected and is fixed by protecting king.
 (test check-moves-tests
@@ -28,9 +28,9 @@
 
 (test mate-moves-tests
   (let ((state (fen->state "3brr2/7b/8/2pN3Q/2p2k2/5P2/4P1KR/2N2RB1 b - - 1 18"))
-        (state2 (fen->state "3brr2/2N4b/8/2p4Q/2p2k2/5P2/4P1KR/2N2RB1 w - - 1 17"))
-        )
-    ))
+        (state2 (fen->state "3brr2/2N4b/8/2p4Q/2p2k2/5P2/4P1KR/2N2RB1 w - - 1 17")))
+    (is-true (matep state))
+    (is-true (matep state2))))
 
 ;; test that white can promote
 (test promotion-tests
@@ -38,81 +38,82 @@
     (is-true
      (member "c7c8Q" (mapcar #'move->coord
                             (legal-moves state))
-             :test #'string=))))
+             :test 'string=))))
 
-(test en-passant-tests
-  (let ((state (fen->state "rnbqkb1r/pppppppp/7n/P7/8/8/1PPPPPPP/RNBQKBNR b KQkq - 0 2")))
-    ))
+;; (test en-passant-tests
+;;       (let ((state (fen->state "rnbqkb1r/pppppppp/7n/P7/8/8/1PPPPPPP/RNBQKBNR b KQkq - 0 2")))
+
+;;     ))
 
 ;;; BOARD TESTS ;;;
 (test board-tests
-  (let ((board (State0x88-board (fen->state "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))))
-    (is-false (white-piece-p +black-pawn+))
-    (is-false (white-piece-p +black-king+))
-    (is-true (white-piece-p +white-pawn+))
-    (is-false (white-piece-p +empty-square+))
-    (is-true (black-piece-p +black-pawn+))
-    (is-false (black-piece-p +white-pawn+))
-    (is-false (black-piece-p +empty-square+))
+  (let ((board (State0x88-board (tursas.state0x88::fen->state "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))))
+    (is-false (tursas.state0x88::white-piece-p tursas.state0x88::+black-pawn+))
+    (is-false (tursas.state0x88::white-piece-p tursas.state0x88::+black-king+))
+    (is-true (tursas.state0x88::white-piece-p tursas.state0x88::+white-pawn+))
+    (is-false (tursas.state0x88::white-piece-p tursas.state0x88::+empty-square+))
+    (is-true (tursas.state0x88::black-piece-p tursas.state0x88::+black-pawn+))
+    (is-false (tursas.state0x88::black-piece-p tursas.state0x88::+white-pawn+))
+    (is-false (tursas.state0x88::black-piece-p tursas.state0x88::+empty-square+))
 
-    (is-true (occupied-by-p (board #x10 +white+)))
-    (is-true (occupied-by-p (board #x00 +white+)))
-    (is-true (occupied-by-p (board #x70 +black+)))
-    (is-false (occupied-by-p (board #x70 +white+)))
-    (is-false (occupied-by-p (board #x40 +white+)))
+    (is-true (tursas.state0x88::occupied-by-p (board #x10 tursas.state0x88::+white+)))
+    (is-true (tursas.state0x88::occupied-by-p (board #x00 tursas.state0x88::+white+)))
+    (is-true (tursas.state0x88::occupied-by-p (board #x70 tursas.state0x88::+black+)))
+    (is-false (tursas.state0x88::occupied-by-p (board #x70 tursas.state0x88::+white+)))
+    (is-false (tursas.state0x88::occupied-by-p (board #x40 tursas.state0x88::+white+)))
 
-    (is (= (opponent +white+) +black+))
-    (is (= (opponent +black+) +white+))
+    (is (= (tursas.state0x88::opponent tursas.state0x88::+white+) tursas.state0x88::+black+))
+    (is (= (tursas.state0x88::opponent tursas.state0x88::+black+) tursas.state0x88::+white+))
 
     ;; (is (promotionp piece move) )
 
-    (is (piece-name +white-king+) #\K)
-    (is (piece-name +black-queen+) #\q)
-    (is (piece-name +white-pawn+) #\p)
-    (is (piece-name +black-king+) #\k)
+    (is (tursas.state0x88::piece-name tursas.state0x88::+white-king+) #\K)
+    (is (tursas.state0x88::piece-name tursas.state0x88::+black-queen+) #\q)
+    (is (tursas.state0x88::piece-name tursas.state0x88::+white-pawn+) #\p)
+    (is (tursas.state0x88::piece-name tursas.state0x88::+black-king+) #\k)
 
-    (is (piece-value #\K) +white-king+)
-    (is (piece-value #\q) +black-queen+)
-    (is (piece-value #\p) +white-pawn+)
-    (is (piece-value #\k) +black-king+)
+    (is (tursas.state0x88::piece-value #\K) tursas.state0x88::+white-king+)
+    (is (tursas.state0x88::piece-value #\q) tursas.state0x88::+black-queen+)
+    (is (tursas.state0x88::piece-value #\p) tursas.state0x88::+white-pawn+)
+    (is (tursas.state0x88::piece-value #\k) tursas.state0x88::+black-king+)
 
-    (is-true (board-index-p #x10))
-    (is-true (board-index-p #x0))
-    (is-true (board-index-p #x77))
-    (is-false (board-index-p #x78))
-    (is-false (board-index-p #x128))
+    (is-true (tursas.state0x88::board-index-p #x10))
+    (is-true (tursas.state0x88::board-index-p #x0))
+    (is-true (tursas.state0x88::board-index-p #x77))
+    (is-false (tursas.state0x88::board-index-p #x78))
+    (is-false (tursas.state0x88::board-index-p #x128))
 
-    (is-true (empty-square-p board #x20))
-    (is-false (empty-square-p board #x00))
-    (is-true (empty-square-p board #x57))
+    (is-true (tursas.state0x88::empty-square-p board #x20))
+    (is-false (tursas.state0x88::empty-square-p board #x00))
+    (is-true (tursas.state0x88::empty-square-p board #x57))
 
-    (is (column #x10) 1)
-    (is (column #x0) 0)
-    (is (column #x60) 6)
+    (is (tursas.state0x88::column #x10) 1)
+    (is (tursas.state0x88::column #x0) 0)
+    (is (tursas.state0x88::column #x60) 6)
 
-    (is (row #x10) 0)
-    (is (row #x22) 2)
-    (is (row #x42) 2)
-    (is (row #x46) 6)
+    (is (tursas.state0x88::row #x10) 0)
+    (is (tursas.state0x88::row #x22) 2)
+    (is (tursas.state0x88::row #x42) 2)
+    (is (tursas.state0x88::row #x46) 6)
 
-    (is-true (same-column-p #x10 #x20))
-    (is-true (same-column-p #x10 #x30))
-    (is-false (same-column-p #x20 #x34))
+    (is-true (tursas.state0x88::same-column-p #x10 #x20))
+    (is-true (tursas.state0x88::same-column-p #x10 #x30))
+    (is-false (tursas.state0x88::same-column-p #x20 #x34))
 
-    (is-true (same-row-p #x10 #x50))
-    (is-true (same-row-p #x10 #x53))
-    (is-false (same-row-p #x11 #x66))
+    (is-true (tursas.state0x88::same-row-p #x10 #x50))
+    (is-true (tursas.state0x88::same-row-p #x10 #x53))
+    (is-false (tursas.state0x88::same-row-p #x11 #x66))
 
-    (is (square-color #x10) +white+)
-    (is (square-color #x11) +black+)
+    (is (tursas.state0x88::square-color #x10) tursas.state0x88::+white+)
+    (is (tursas.state0x88::square-color #x11) tursas.state0x88::+black+)
 
-    (is-true (same-color-p #x10 #x12))
+    (is-true (tursas.state0x88::same-color-p #x10 #x12))
 
-    (is-true (board-occupied-p board #x10))
+    (is-true (tursas.state0x88::board-occupied-p board #x10))
 
-    (is (board-ref board #x10) +white-pawn+)
-    (is (board-ref board #x30) +empty-square+)
+    (is (tursas.state0x88::board-ref board #x10) tursas.state0x88::+white-pawn+)
+    (is (tursas.state0x88::board-ref board #x30) tursas.state0x88::+empty-square+)
 
-    (is-false (fill-square! board #x45 20))
+    (is-false (tursas.state0x88::fill-square! board #x45 20))
 
-    (is (king-index board +white+) #x44)))
+    (is (tursas.state0x88::king-index board tursas.state0x88::+white+) #x44)))
