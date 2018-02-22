68d89560bce759a8dba4e455e5d36af9c36fda32
Timo Myyrä <timo.myyra@wickedbsd.net>
2018-02-23 00:17:20 +0200

    remove premature optimizations from alpha-beta

--- core.lisp
+++ core.lisp
@@ -235,26 +235,18 @@
   "Find the best state, from initial state by searching to given depth
    and backing up values using cutoff whenever possible.
    Based on alpha-beta presented in PAIP"
-  (declare (fixnum alpha beta depth)
-           (optimize (speed 3)
-                     (safety 0)))
   (labels ((f (states best-state ac depth)
-             (declare (fixnum ac depth))
              (if (null states)
                  (values ac best-state)
-                 (let ((value (the fixnum (- (the fixnum (alpha-beta (the fixnum (- beta))
-                                                                     (the fixnum (- ac))
-                                                                     depth
-                                                                     eval-fn
-                                                                     (first states)))))))
+                 (let ((value (- (alpha-beta (- beta) (- ac) depth eval-fn (first states)))))
                    (cond ((>= ac beta) (values ac best-state))
                          ((> value ac) (f (rest states) (first states) value depth))
                          (t (f (rest states) best-state ac depth)))))))
     (cond ((game-end-p state) (values (game-score state) nil))
-          ((zerop depth) (values (funcall (the function eval-fn) state) nil))
+          ((zerop depth) (values (funcall eval-fn state) nil))
           (t (if-let ((children (legal-states state)))
                (f children (first children) alpha (1- depth))
-               (f nil nil (funcall (the function eval-fn) state) 0))))))
+               (f nil nil (funcall eval-fn state) 0))))))
 
 (defun load-game ()
   "Loads the game-state from file to resume the previous game."
