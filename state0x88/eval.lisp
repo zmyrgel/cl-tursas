a34a83ec4e124e69c721e8d716cfe917630e894d
Timo Myyrä <timo.myyra@bittivirhe.fi>
2019-04-12 07:30:09 +0300

    fix piece index-score calculation
    last version reversed the full 0x88 board which caused invalid results for black
    player.

--- state0x88/eval.lisp
+++ state0x88/eval.lisp
@@ -29,141 +29,126 @@
                 :initial-contents (loop for rank in (group score-list 8)
                                         append (append rank (make-list 8 :initial-element 0))))))
 
-(define-constant +white-pawn-table+
-    (make-table '(0   0   0   0   0   0   0   0
-                  5   5   5   0   0   5   5   5
-                  0   0   5  15  15   5   0   0
-                  5   5  10  15  15  10   5   5
-                  0   0   5  10  10   5   0   0
-                  0   0   0   0   0   0   0   0
-                  0   0   0   0   0   0   0   0
-                  0   0   0   0   0   0   0   0))
-  :test #'equalp)
-(define-constant +black-pawn-table+ (reverse +white-pawn-table+)
-  :test #'equalp)
+(let* ((pawn-table '(0   0   0   0   0   0   0   0
+                    5   5   5   0   0   5   5   5
+                    0   0   5  15  15   5   0   0
+                    5   5  10  15  15  10   5   5
+                    0   0   5  10  10   5   0   0
+                    0   0   0   0   0   0   0   0
+                    0   0   0   0   0   0   0   0
+                    0   0   0   0   0   0   0   0))
+      (rook-table '(5  0  5  0  0  5  0  5
+                    0  0  0  0  0  0  0  0
+                    0  0  0  0  0  0  0  0
+                    0  0  0  0  0  0  0  0
+                    0  0  0  0  0  0  0  0
+                    0  0  0  0  0  0  0  0
+                    0  0  0  0  0  0  0  0
+                    0  0  0  0  0  0  0  0))
+      (rook-end-table '(0  0  0  0  0  0  0  0
+                        0  0  0  0  0  0  0  0
+                        0  0  0  0  0  0  0  0
+                        0  0  0  0  0  0  0  0
+                        0  0  0  0  0  0  0  0
+                        0  0  0  0  0  0  0  0
+                        0  0  0  0  0  0  0  0
+                        0  0  0  0  0  0  0  0))
+      (knight-table '(-10 -5 -5  -5  -5  -5  -5 -10
+                      -5  -5  0   5   5   0  -5 -5
+                      -5   5  5   10  10  5   5 -5
+                      -5   0  10  10  10  10  0 -5
+                      -5   0  10  10  10  10  0 -5
+                      -5   5  5   10  10  10  5 -5
+                      -5  -5  0   5   5   0  -5 -5
+                      -10 -5  -5  -5  -5  -5 -5 -10))
+      (bishop-table '(-15 -10 -10 -10 -10 -10 -10 -15
+                      -10   0   0   0   0   0   0 -10
+                      -10   0   5  10  10   5   0 -10
+                      -10   5   5  10  10   5   5 -10
+                      -10   0  10  10  10  10   0 -10
+                      -10  10  10  10  10  10  10 -10
+                      -10   5   0   0   0   0   5 -10
+                      -15 -10 -10 -10 -10 -10 -10 -15))
+      (king-table '(  0  -5   5   0   0  -5   5   0
+                    -10 -10 -10 -10 -10 -10 -10 -10
+                    -10 -10 -10 -10 -10 -10 -10 -10
+                    -10 -10 -10 -10 -10 -10 -10 -10
+                    -10 -10 -10 -10 -10 -10 -10 -10
+                    -10 -10 -10 -10 -10 -10 -10 -10
+                    -10 -10 -10 -10 -10 -10 -10 -10
+                    -10 -10 -10 -10 -10 -10 -10 -10))
+      (king-end-table '(-15 -10 -10 -10 -10 -10 -10 -15
+                        -10 -10  -5   0   0  -5 -10 -10
+                        -10  -5   5  10  10   5  -5 -10
+                        -10  -5  10  15  15  10  -5 -10
+                        -10  -5  10  15  15  10  -5 -10
+                        -10  -5   5  10  10   5  -5 -10
+                        -10 -10  -5   0   0  -5 -10 -10
+                        -15 -10 -10 -10 -10 -10 -10 -15))
 
-(define-constant +white-rook-table+
-    (make-table '(5  0  5  0  0  5  0  5
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0))
-  :test #'equalp)
-(define-constant +black-rook-table+ (reverse +white-rook-table+)
-  :test #'equalp)
+       (white-pawn-table (make-table pawn-table))
+       (black-pawn-table (reverse (make-table pawn-table)))
+       (white-rook-table (make-table rook-table))
+       (black-rook-table (make-table (reverse rook-table)))
+       (white-rook-end-table (make-table rook-end-table))
+       (black-rook-end-table (make-table (reverse rook-end-table)))
+       (white-knight-table (make-table knight-table))
+       (black-knight-table (make-table (reverse knight-table)))
+       (white-bishop-table (make-table bishop-table))
+       (black-bishop-table (make-table (reverse bishop-table)))
+       (white-king-table (make-table king-table))
+       (black-king-table (make-table (reverse king-table)))
+       (white-king-end-game-table (make-table king-end-table))
+       (black-king-end-game-table (make-table (reverse king-end-table))))
 
-(define-constant +white-rook-end-table+
-    (make-table '(0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0
-                  0  0  0  0  0  0  0  0))
-  :test #'equalp)
-(define-constant +black-rook-end-table+ (reverse +white-rook-end-table+)
-  :test #'equalp)
-
-(define-constant +white-knight-table+
-    (make-table '(-10 -5 -5  -5  -5  -5  -5 -10
-                  -5  -5  0   5   5   0  -5 -5
-                  -5   5  5   10  10  5   5 -5
-                  -5   0  10  10  10  10  0 -5
-                  -5   0  10  10  10  10  0 -5
-                  -5   5  5   10  10  10  5 -5
-                  -5  -5  0   5   5   0  -5 -5
-                  -10 -5  -5  -5  -5  -5 -5 -10))
-  :test #'equalp)
-(define-constant +black-knight-table+ (reverse +white-knight-table+)
-  :test #'equalp)
-
-(define-constant +white-bishop-table+
-    (make-table '(-15 -10 -10 -10 -10 -10 -10 -15
-                  -10   0   0   0   0   0   0 -10
-                  -10   0   5  10  10   5   0 -10
-                  -10   5   5  10  10   5   5 -10
-                  -10   0  10  10  10  10   0 -10
-                  -10  10  10  10  10  10  10 -10
-                  -10   5   0   0   0   0   5 -10
-                  -15 -10 -10 -10 -10 -10 -10 -15))
-  :test #'equalp)
-(define-constant +black-bishop-table+ (reverse +white-bishop-table+)
-  :test #'equalp)
-
-(define-constant +white-king-table+
-    (make-table '(  0  -5   5   0   0  -5   5   0
-                  -10 -10 -10 -10 -10 -10 -10 -10
-                  -10 -10 -10 -10 -10 -10 -10 -10
-                  -10 -10 -10 -10 -10 -10 -10 -10
-                  -10 -10 -10 -10 -10 -10 -10 -10
-                  -10 -10 -10 -10 -10 -10 -10 -10
-                  -10 -10 -10 -10 -10 -10 -10 -10
-                  -10 -10 -10 -10 -10 -10 -10 -10))
-  :test #'equalp)
-(define-constant +black-king-table+ (reverse +white-king-table+)
-  :test #'equalp)
-
-(define-constant +white-king-end-game-table+
-    (make-table '(-15 -10 -10 -10 -10 -10 -10 -15
-                  -10 -10  -5   0   0  -5 -10 -10
-                  -10  -5   5  10  10   5  -5 -10
-                  -10  -5  10  15  15  10  -5 -10
-                  -10  -5  10  15  15  10  -5 -10
-                  -10  -5   5  10  10   5  -5 -10
-                  -10 -10  -5   0   0  -5 -10 -10
-                  -15 -10 -10 -10 -10 -10 -10 -15))
-  :test #'equalp)
-(define-constant +black-king-end-game-table+ (reverse +white-king-end-game-table+)
-  :test #'equalp)
+  (defun index-score (piece index game-situation)
+    "Checks piece-specific index score"
+    (cond ((= piece +white-pawn+)
+           (board-ref white-pawn-table index))
+          ((= piece +black-pawn+)
+           (board-ref black-pawn-table index))
+          ((= piece +white-knight+)
+           (board-ref white-knight-table index))
+          ((= piece +black-knight+)
+           (board-ref black-knight-table index))
+          ((= piece +white-bishop+)
+           (board-ref white-bishop-table index))
+          ((= piece +black-bishop+)
+           (board-ref black-bishop-table index))
+          ((= piece +white-rook+)
+           (if (= game-situation +end-game+)
+               (board-ref white-rook-end-table index)
+               (board-ref white-rook-table index)))
+          ((= piece +black-rook+)
+           (if (= game-situation +end-game+)
+               (board-ref black-rook-end-table index)
+               (board-ref black-rook-table index)))
+          ((= piece +white-king+)
+           (if (= game-situation +end-game+)
+               (board-ref white-king-end-game-table index)
+               (board-ref white-king-table index)))
+          ((= piece +black-king+)
+           (if (= game-situation +end-game+)
+               (board-ref black-king-end-game-table index)
+               (board-ref black-king-table index)))
+          (t
+           0))))
 
 (defun material-value (piece)
-  "Gives material value for given piece."
-  (cond ((or (= piece +white-pawn+) (= piece +black-pawn+))
-         +pawn-value+)
-        ((or (= piece +white-knight+) (= piece +black-knight+))
-         +knight-value+)
-        ((or (= piece +white-bishop+) (= piece +black-bishop+))
-         +bishop-value+)
-        ((or (= piece +white-rook+) (= piece +black-rook+))
-         +rook-value+)
-        ((or (= piece +white-queen+) (= piece +black-queen+))
-         +queen-value+)
-        ((or (= piece +white-king+) (= piece +black-king+))
-         +king-value+)
-        (t 0)))
-
-(defun index-score (piece index game-situation)
-  "Checks piece-specific index score"
-  (cond ((= piece +white-pawn+)
-         (board-ref +white-pawn-table+ index))
-        ((= piece +black-pawn+)
-         (board-ref +black-pawn-table+ index))
-        ((= piece +white-knight+)
-         (board-ref +white-knight-table+ index))
-        ((= piece +black-knight+)
-         (board-ref +black-knight-table+ index))
-        ((= piece +white-bishop+)
-         (board-ref +white-bishop-table+ index))
-        ((= piece +black-bishop+)
-         (board-ref +black-bishop-table+ index))
-        ((= piece +white-rook+)
-         (board-ref +white-rook-table+ index))
-        ((= piece +black-rook+)
-         (board-ref +black-rook-table+ index))
-        ((= piece +white-king+)
-         (if (= game-situation +end-game+)
-             (board-ref +white-king-end-game-table+ index)
-             (board-ref +white-king-table+ index)))
-        ((= piece +black-king+)
-         (if (= game-situation +end-game+)
-             (board-ref +black-king-end-game-table+ index)
-             (board-ref +black-king-table+ index)))
-        (t
-         0)))
+    "Gives material value for given piece."
+    (cond ((or (= piece +white-pawn+) (= piece +black-pawn+))
+           +pawn-value+)
+          ((or (= piece +white-knight+) (= piece +black-knight+))
+           +knight-value+)
+          ((or (= piece +white-bishop+) (= piece +black-bishop+))
+           +bishop-value+)
+          ((or (= piece +white-rook+) (= piece +black-rook+))
+           +rook-value+)
+          ((or (= piece +white-queen+) (= piece +black-queen+))
+           +queen-value+)
+          ((or (= piece +white-king+) (= piece +black-king+))
+           +king-value+)
+          (t 0)))
 
 (defun score (pieces situation)
   "Calculates score for side."
